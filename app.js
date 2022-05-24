3359e1d1e5fbbbb3a055969e120a18ab3ee96ebf
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-24 15:47:37 +0430

    add function setDisplay

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 let display = document.querySelector(".display");
+let displayResult = document.querySelector('.display-result')
 let clearbtn = document.querySelector(".clear");
 let deletebtn = document.querySelector(".delete");
 let remainder = document.querySelector(".remainde");
 let multiplier = document.querySelector(".multiplier");
-let number7 = document.querySelector(".seven");
-let number8 = document.querySelector(".eight");
-let number9 = document.querySelector(".nine");
 let divider = document.querySelector(".divider");
-let number4 = document.querySelector(".four");
-let number5 = document.querySelector(".five");
-let number6 = document.querySelector(".sixe");
 let total = document.querySelector(".total");
+let subtractor = document.querySelector(".subtraction");
+let equal = document.querySelector(".equal");
+let dot = document.querySelector(".dot");
+let number0 = document.querySelector(".zero");
 let number1 = document.querySelector(".one");
 let numbre2 = document.querySelector(".two");
 let number3 = document.querySelector(".three");
-let subtractor = document.querySelector(".subtraction");
-let number0 = document.querySelector(".zero");
-let dot = document.querySelector(".dot");
-let equal = document.querySelector(".equal");
+let number4 = document.querySelector(".four");
+let number5 = document.querySelector(".five");
+let number6 = document.querySelector(".sixe");
+let number7 = document.querySelector(".seven");
+let number8 = document.querySelector(".eight");
+let number9 = document.querySelector(".nine");
+
+
+number1.addEventListener('click', () => setDisplay(number1 , displayResult))
+number0.addEventListener('click', () => setDisplay(number0 , displayResult))
+
+
+
+function setDisplay(number , displayResult) {
+    displayResult.innerHTML = `${number.textContent}`
+}
+
