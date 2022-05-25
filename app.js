1e0ba562d62462bc71ad18a120664ec08975a9bb
Amir Hossein Heydari <amir123fg5@gmail.com>
2022-05-25 10:12:20 +0430

    complete display result of botton

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let display = document.querySelector(".display");
+
 let displayResult = document.querySelector('.display-result')
 let clearbtn = document.querySelector(".clear");
 let deletebtn = document.querySelector(".delete");
@@ -9,24 +9,33 @@ let total = document.querySelector(".total");
 let subtractor = document.querySelector(".subtraction");
 let equal = document.querySelector(".equal");
 let dot = document.querySelector(".dot");
-let number0 = document.querySelector(".zero");
-let number1 = document.querySelector(".one");
-let numbre2 = document.querySelector(".two");
-let number3 = document.querySelector(".three");
-let number4 = document.querySelector(".four");
-let number5 = document.querySelector(".five");
-let number6 = document.querySelector(".sixe");
-let number7 = document.querySelector(".seven");
-let number8 = document.querySelector(".eight");
-let number9 = document.querySelector(".nine");
 
 
-number1.addEventListener('click', () => setDisplay(number1 , displayResult))
-number0.addEventListener('click', () => setDisplay(number0 , displayResult))
+let buttons = document.querySelectorAll(".item3")
 
+buttons.forEach((div) => {
+    div.addEventListener("click", () => setDisplay(div , displayResult))
+})
 
 
 function setDisplay(number , displayResult) {
-    displayResult.innerHTML = `${number.textContent}`
+    let displayNum = displayResult.textContent
+    displayResult.textContent = `${number.textContent}`
+    displayResult.textContent = displayNum + `${number.textContent}`
+    if (displayNum === "0") {
+        resetDisplay(number, displayResult)
+    }
+    
 }
+
+function resetDisplay(number, displayResult) {
+    displayResult.textContent = `${number.textContent}`
+}
+
+
+
+
+
+
+
 
